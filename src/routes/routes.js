71ac651296da37ad67330d94992cd30971491ae6
Wendryl <wendryl10000@gmail.com>
2021-04-18 11:37:13 -0300

    add product crud routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,11 +7,19 @@ const company = require('../controllers/company');
 
 router.post('/auth', user.authenticate);
 
+// products
 router.get('/companies', auth.verifyToken, company.list);
 router.get('/companies/:id', auth.verifyToken, company.find);
 router.post('/companies', auth.verifyToken, company.store);
 router.put('/companies/:id', auth.verifyToken, company.alter);
 router.delete('/companies/:id', auth.verifyToken, company.destroy);
+
+// Products
+router.get('/products', auth.verifyToken, product.list);
+router.get('/products/:id', auth.verifyToken, product.find);
+router.post('/products', auth.verifyToken, product.store);
+router.put('/products/:id', auth.verifyToken, product.alter);
+router.delete('/products/:id', auth.verifyToken, product.destroy);
 
 
 module.exports = router 
