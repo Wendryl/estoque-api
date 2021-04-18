d91126de9cba7c7434d194ff2d8f8d76c8359fd0
Wendryl <wendryl10000@gmail.com>
2021-04-18 11:49:00 -0300

    add category controller

---
+++ src/controllers/category.js
@@ -0,0 +1,97 @@
+const knex = require('../config/db.config');
+
+class CategoryController {
+
+  async list(req, res) {
+
+    try {
+
+      const result = await knex('Category').select('*')
+
+      return res.json(result).end();
+
+    } catch(e) {
+      console.error(e);
+      res.status(500).end();
+    }
+
+  }
+
+  async find(req, res) {
+
+    try {
+
+      const categoryId = req.params.id;
+
+      const result = await knex('Category').select('*')
+        .where('id', categoryId)
+
+      if(result.length < 1) {
+        return res.status(404).end();
+      }
+
+      return res.json(result).end();
+
+    } catch(e) {
+      console.error(e);
+      res.status(500).end();
+    }
+
+  }
+
+  async store(req, res) {
+
+    try {
+
+      const product = req.body;
+      const result = await knex('Category').insert(product, 'id')
+
+      return res.json(result).end();
+
+    } catch(e) {
+      console.error(e);
+      res.status(500).end();
+    }
+
+  }
+
+  async alter(req, res) {
+
+    try {
+
+      const categoryId = req.params.id;
+      const product = req.body;
+
+      const result = await knex('Category').update(product)
+        .where('id', categoryId)
+
+      return res.json(result).end();
+
+    } catch(e) {
+      console.error(e);
+      res.status(500).end();
+    }
+
+  }
+
+  async destroy(req, res) {
+
+    try {
+
+      const categoryId = req.params.id;
+
+      const result = await knex('Category').delete('id')
+        .where('id', categoryId)
+
+      return res.json(result).end();
+
+    } catch(e) {
+      console.error(e);
+      res.status(500).end();
+    }
+
+  }
+
+}
+
+module.exports = new CategoryController();
