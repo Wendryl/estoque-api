8285f436f5c468967a6ea8002768204a0ec4f388
Wendryl <wendryl10000@gmail.com>
2021-04-18 11:49:40 -0300

    add categories routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const auth = require('../controllers/auth');
 const product = require('../controllers/product');
 const company = require('../controllers/company');
+const category  = require('../controllers/category');
 
 router.post('/auth', user.authenticate);
 
-// products
+// Companies
 router.get('/companies', auth.verifyToken, company.list);
 router.get('/companies/:id', auth.verifyToken, company.find);
 router.post('/companies', auth.verifyToken, company.store);
@@ -20,6 +21,13 @@ router.get('/products/:id', auth.verifyToken, product.find);
 router.post('/products', auth.verifyToken, product.store);
 router.put('/products/:id', auth.verifyToken, product.alter);
 router.delete('/products/:id', auth.verifyToken, product.destroy);
+
+// Categories
+router.get('/categories', auth.verifyToken, category.list);
+router.get('/categories/:id', auth.verifyToken, category.find);
+router.post('/categories', auth.verifyToken, category.store);
+router.put('/categories/:id', auth.verifyToken, category.alter);
+router.delete('/categories/:id', auth.verifyToken, category.destroy);
 
 
 module.exports = router 
